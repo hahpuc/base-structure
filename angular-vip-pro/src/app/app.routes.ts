5677ccd36418d653468bf84e011598bbfeba7968
Nguyen Thanh Long <nguyenthanhlong0420@gmail.com>
2025-09-18 14:00:08 +0700

    AngularVP: Add Ant Design

--- angular-vip-pro/src/app/app.routes.ts
+++ angular-vip-pro/src/app/app.routes.ts
@@ -1,22 +1,23 @@
 import { Routes } from "@angular/router";
-import { EcommerceComponent } from "./pages/dashboard/ecommerce/ecommerce.component";
-import { ProfileComponent } from "./pages/profile/profile.component";
-import { FormElementsComponent } from "./pages/forms/form-elements/form-elements.component";
-import { BasicTablesComponent } from "./pages/tables/basic-tables/basic-tables.component";
+import { SignInComponent } from "./pages/auth-pages/sign-in/sign-in.component";
+import { SignUpComponent } from "./pages/auth-pages/sign-up/sign-up.component";
 import { BlankComponent } from "./pages/blank/blank.component";
-import { NotFoundComponent } from "./pages/other-page/not-found/not-found.component";
-import { AppLayoutComponent } from "./shared/layout/app-layout/app-layout.component";
-import { InvoicesComponent } from "./pages/invoices/invoices.component";
-import { LineChartComponent } from "./pages/charts/line-chart/line-chart.component";
 import { BarChartComponent } from "./pages/charts/bar-chart/bar-chart.component";
+import { LineChartComponent } from "./pages/charts/line-chart/line-chart.component";
+import { EcommerceComponent } from "./pages/dashboard/ecommerce/ecommerce.component";
+import { NzDemoFormNormalLoginComponent } from "./pages/forms-antd/forms-antd.component";
+import { FormElementsComponent } from "./pages/forms/form-elements/form-elements.component";
+import { InvoicesComponent } from "./pages/invoices/invoices.component";
+import { NotFoundComponent } from "./pages/other-page/not-found/not-found.component";
+import { ProfileComponent } from "./pages/profile/profile.component";
+import { BasicTablesComponent } from "./pages/tables/basic-tables/basic-tables.component";
 import { AlertsComponent } from "./pages/ui-elements/alerts/alerts.component";
 import { AvatarElementComponent } from "./pages/ui-elements/avatar-element/avatar-element.component";
 import { BadgesComponent } from "./pages/ui-elements/badges/badges.component";
 import { ButtonsComponent } from "./pages/ui-elements/buttons/buttons.component";
 import { ImagesComponent } from "./pages/ui-elements/images/images.component";
 import { VideosComponent } from "./pages/ui-elements/videos/videos.component";
-import { SignInComponent } from "./pages/auth-pages/sign-in/sign-in.component";
-import { SignUpComponent } from "./pages/auth-pages/sign-up/sign-up.component";
+import { AppLayoutComponent } from "./shared/layout/app-layout/app-layout.component";
 
 export const routes: Routes = [
   {
@@ -108,6 +109,12 @@ export const routes: Routes = [
         component: VideosComponent,
         title:
           "Angular Videos Dashboard | TailAdmin - Angular Admin Dashboard Template",
+      },
+      {
+        path: "form-antd",
+        component: NzDemoFormNormalLoginComponent,
+        title:
+          "Angular Form AntD Dashboard | TailAdmin - Angular Admin Dashboard Template",
       },
     ],
   },
---
+++ angular-vip-pro/src/app/pages/forms-antd/forms-antd.component.ts
@@ -0,0 +1,114 @@
+import { Component, inject } from "@angular/core";
+import {
+  NonNullableFormBuilder,
+  ReactiveFormsModule,
+  Validators,
+} from "@angular/forms";
+
+import { NzButtonModule } from "ng-zorro-antd/button";
+import { NzCheckboxModule } from "ng-zorro-antd/checkbox";
+import { NzFormModule } from "ng-zorro-antd/form";
+import { NzInputModule } from "ng-zorro-antd/input";
+
+@Component({
+  selector: "nz-demo-form-normal-login",
+  imports: [
+    ReactiveFormsModule,
+    NzButtonModule,
+    NzCheckboxModule,
+    NzFormModule,
+    NzInputModule,
+  ],
+  template: `
+    <form
+      nz-form
+      [formGroup]="validateForm"
+      class="login-form"
+      (ngSubmit)="submitForm()"
+    >
+      <nz-form-item>
+        <nz-form-control nzErrorTip="Please input your username!">
+          <nz-input-group>
+            <input
+              type="text"
+              nz-input
+              formControlName="username"
+              placeholder="Username"
+            />
+          </nz-input-group>
+        </nz-form-control>
+      </nz-form-item>
+      <nz-form-item>
+        <nz-form-control nzErrorTip="Please input your Password!">
+          <nz-input-group>
+            <input
+              type="password"
+              nz-input
+              formControlName="password"
+              placeholder="Password"
+            />
+          </nz-input-group>
+        </nz-form-control>
+      </nz-form-item>
+      <div nz-row class="login-form-margin">
+        <div nz-col [nzSpan]="12">
+          <label nz-checkbox formControlName="remember">
+            <span>Remember me</span>
+          </label>
+        </div>
+        <div nz-col [nzSpan]="12">
+          <a class="login-form-forgot">Forgot password</a>
+        </div>
+      </div>
+      <button
+        nz-button
+        class="login-form-button login-form-margin"
+        [nzType]="'primary'"
+      >
+        Log in
+      </button>
+      Or
+      <a>register now!</a>
+    </form>
+  `,
+  styles: [
+    `
+      .login-form {
+        max-width: 300px;
+      }
+
+      .login-form-margin {
+        margin-bottom: 16px;
+      }
+
+      .login-form-forgot {
+        float: right;
+      }
+
+      .login-form-button {
+        width: 100%;
+      }
+    `,
+  ],
+})
+export class NzDemoFormNormalLoginComponent {
+  private fb = inject(NonNullableFormBuilder);
+  validateForm = this.fb.group({
+    username: this.fb.control("", [Validators.required]),
+    password: this.fb.control("", [Validators.required]),
+    remember: this.fb.control(true),
+  });
+
+  submitForm(): void {
+    if (this.validateForm.valid) {
+      console.log("submit", this.validateForm.value);
+    } else {
+      Object.values(this.validateForm.controls).forEach((control) => {
+        if (control.invalid) {
+          control.markAsDirty();
+          control.updateValueAndValidity({ onlySelf: true });
+        }
+      });
+    }
+  }
+}
