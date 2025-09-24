c4c922fdff25502dda4e5b0d44b93a18d794a1c7
Nguyen Thanh Long <nguyenthanhlong0420@gmail.com>
2025-09-24 10:26:51 +0700

    AngularVP: update locale nz-i18n

--- angular-vip-pro/src/app/app.config.ts
+++ angular-vip-pro/src/app/app.config.ts
@@ -1,14 +1,15 @@
-import {
-  provideHttpClient,
-  withFetch,
-  withInterceptors,
-} from "@angular/common/http";
-import { ApplicationConfig, provideZoneChangeDetection } from "@angular/core";
-import { provideAnimations } from "@angular/platform-browser/animations";
-import { provideRouter } from "@angular/router";
+import { registerLocaleData } from '@angular/common';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+import en from '@angular/common/locales/en';
+import { ApplicationConfig, inject, LOCALE_ID, provideZoneChangeDetection } from '@angular/core';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideRouter } from '@angular/router';
+import { en_US, NZ_I18N } from 'ng-zorro-antd/i18n';
 
-import { routes } from "./app.routes";
-import { httpCustomInterceptor } from "./shared/http-interceptor/http-custom-interceptor";
+import { routes } from './app.routes';
+import { httpCustomInterceptor } from './shared/http-interceptor/http-custom-interceptor';
+
+registerLocaleData(en);
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -16,5 +17,19 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimations(),
+
+    {
+      provide: NZ_I18N,
+      useFactory: () => {
+        const localId = inject(LOCALE_ID);
+        switch (localId) {
+          case 'en':
+            return en_US;
+
+          default:
+            return en_US;
+        }
+      },
+    },
   ],
 };
